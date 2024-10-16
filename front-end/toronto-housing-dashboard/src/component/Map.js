2fe28544668af5c461c4700488684dc2d8699a49
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2024-10-15 20:58:02 -0400

    adding up the info

--- front-end/toronto-housing-dashboard/src/component/Map.js
+++ front-end/toronto-housing-dashboard/src/component/Map.js
@@ -161,7 +161,7 @@ const Map = () => {
 
     const fetchCommunityData = async (communityName) => {
         try {
-            const response = await axios.post('https://toronto-house-prices-predection-app.vercel.app:5000/community-data', {
+            const response = await axios.post('http://192.168.2.178:5001/community-data', {
                 community: communityName,
             });
             setHousingData(response.data);
