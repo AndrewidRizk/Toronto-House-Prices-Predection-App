7091dd06b2181b1864648d1f1c89b4eef3d36dc2
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2025-01-15 01:54:05 -0500

    Update Map.js

--- front-end/toronto-housing-dashboard/src/component/Map.js
+++ front-end/toronto-housing-dashboard/src/component/Map.js
@@ -163,7 +163,7 @@ const Map = () => {
     const fetchCommunityData = async (communityName) => {
         try {
             
-            const response = await axios.post('https://2111-70-50-135-241.ngrok-free.app/community-data', {
+            const response = await axios.post('https://ca35-70-50-135-241.ngrok-free.app/community-data', {
                 community: communityName,
             });
             setHousingData(response.data);
